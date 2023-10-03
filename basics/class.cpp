4e1e651c96911554c5b4ba00ca860d966a880843
NicoElbers <Ryojipog@gmail.com>
2023-10-03 18:32:40 +0200

    did some more work on cpp

--- basics/class.cpp
+++ basics/class.cpp
@@ -5,12 +5,17 @@ private:
   double radius;
 
 public:
+  Circle();
+  Circle(int);
+  Circle(Circle const &);
+  ~Circle(){};
   void setRadius(double r);
   double getRadius();
 };
 
 int main() {
   Circle c1;
+  Circle c2(10);
   c1.setRadius(69);
   std::cout << c1.getRadius() << std::endl;
 }
@@ -18,3 +23,9 @@ int main() {
 void Circle ::setRadius(double r) { radius = r; }
 
 double Circle ::getRadius() { return radius; }
+
+Circle ::Circle() { radius = 0; }
+
+Circle ::Circle(int r) { radius = r; }
+
+Circle ::Circle(Circle const &o) { radius = o.radius; }
